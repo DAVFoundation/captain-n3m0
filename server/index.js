d2986cd09fd6b230c266f07a4f05066e285a202a
Tal Ater <tal@talater.com>
2018-08-12 18:57:11 +0300

    Implemented routes for creating a need and getting status

--- server/index.js
+++ server/index.js
@@ -1,10 +1,44 @@
 const express = require('express');
+const storage = require('./lib/storage');
+const key = process.env.KEY;
+
+// Start Express
 const app = express();
 const port = process.env.WEB_SERVER_PORT || 8888;
+
+// Validate params
+app.param('key', function(req, res, next, id) {
+  if (id !== key) {
+    next(new Error('Invalid Key'));
+  } else {
+    next();
+  }
+});
+
+app.param('mission_id', async function(req, res, next, id) {
+  try {
+    const mission = await storage.getMission(id);
+    req.mission = mission;
+    next();
+  } catch(e) {
+    next(new Error('Invalid mission id'));
+  }
+});
 
 // Define routes
 app.get('/healthy', (req, res) => {
   res.send('hello world');
+});
+
+app.get('/need/:key', async (req, res) => {
+  const missionId = await storage.createNeed();
+  res.send({
+    missionId
+  });
+});
+
+app.get('/status/:key/:mission_id', async (req, res) => {
+  res.send(req.mission);
 });
 
 // Start server
