1c2a2dc2a06f78ff5b06e3aadec089ea22038342
Tal Ater <tal@talater.com>
2018-08-12 18:56:54 +0300

    Added storage.js with getMission() and createNeed()

---
+++ server/lib/storage.js
@@ -0,0 +1,65 @@
+const mysql = require('mysql');
+
+const host = process.env.MYSQL_HOST;
+const user = process.env.MYSQL_USER;
+const password = process.env.MYSQL_PASSWORD;
+const database = process.env.MYSQL_DB;
+
+const createConnection = (multipleStatements = false) => {
+  return mysql.createConnection({
+    host: host,
+    user: user,
+    password: password,
+    database: database,
+    timezone: 'utc',
+    multipleStatements: multipleStatements
+  });
+}
+
+const createNeed = async () => {
+  return new Promise((resolve, reject) => {
+    const connection = createConnection(true);
+    connection.query(
+      `INSERT INTO missions (need_created_at) VALUES (NOW())`,
+      function(error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(
+            results.insertId
+          );
+        }
+        connection.destroy();
+      }
+    );
+  });
+}
+
+const getMission = async(id) => {
+  return new Promise((resolve, reject) => {
+    const connection = createConnection(true);
+    connection.query(
+      `SELECT * FROM missions WHERE id = ?`,
+      [id],
+      function(error, results, fields) {
+        if (error || results.length !== 1) {
+          reject(error);
+        } else {
+          resolve(
+            {
+              id: results[0].id,
+              need_created_at: results[0].need_created_at,
+              state: results[0].state,
+            }
+          );
+        }
+        connection.destroy();
+      }
+    );
+  });
+}
+
+module.exports = {
+  createNeed,
+  getMission,
+};
