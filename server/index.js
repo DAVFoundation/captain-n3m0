2c92f937840e0b3b5343ce2dae21cc05e37ec4ad
Hai <hai.alaluf@gmail.com>
2018-08-16 17:43:24 +0300

    bug fixes

--- server/index.js
+++ server/index.js
@@ -1,26 +1,18 @@
 const { updateMissionState } = require('./model/mission');
 const storage = require('./lib/storage');
-const util = require('util');
 
-try {
-  exports.handler = async function (event, context, callback) {
-    try {
-      const key = process.env.KEY;
-      if (isKeyValid(key, event)) {
-        const path = event.path.substring(1);
-        await mainHandler[path]({event, context, callback});
-      } else {
-        callback(new Error('Invalid Key'));
-      }
-    } catch (error) {
-      console.log(util.inspect(error));
-    }
-  };
-} catch (error) {
-  console.log(util.inspect(error));
-}
+exports.handler = async function (event, context, callback) {
+  const response = generateResponse(callback);
+  const key = process.env.KEY;
+  if (isKeyValid(key, event)) {
+    const path = event.path.substring(1);
+    await mainHandler[path]({event, context, response});
+  } else {
+    response.error(new Error('Invalid Key'));
+  }
+};
 
-const isKeyValid = (key, event) => !event.pathParameters || !event.pathParameters.key || event.pathParameters.key === key;
+const isKeyValid = (key, event) => !event.pathParameters || event.pathParameters.key || event.pathParameters.key === key;
 
 const getMission = async (event) => {
   try {
@@ -29,29 +21,46 @@ const getMission = async (event) => {
   } catch (e) {
     throw new Error('Invalid mission id');
   }
-}
+};
 
 const mainHandler = {
-  healthy: ({callback}) => callback(null, 'Hello World!'),
-  need: ({callback}) => {
+  healthy: ({response}) => response.success('Hello World!'),
+  need: async ({response}) => {
     const missionId = await storage.createNeed();
-    callback(null, { missionId });
+    response.success({ missionId });
   },
-  status: ({event, callback}) => {
+  status: async ({event, response}) => {
     try {
       const mission = await getMission(event);
-      callback(null, mission);
+      response.success(mission);
     } catch (err) {
-      callback(err);
+      response.error(err);
     }
   },
-  begin_charging: ({event, callback}) => {
+  begin_charging: async ({event, response}) => {
     try {
       let mission = await getMission(event);
       mission = await updateMissionState(mission, 'charging');
-      callback(null, mission);
+      response.success(mission);
     } catch (err) {
-      callback(err);
+      response.error(err);
     }
   }
-}
+};
+
+const generateResponse = (callback) => ({
+  error: (msg) => {
+    callback({
+      statusCode: 400,
+      body: JSON.stringify(msg),
+      headers: {'Content-Type': 'application/json'}
+    });
+  },
+  success: (msg) => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(msg),
+      headers: {'Content-Type': 'application/json'}
+    });
+  }
+});
