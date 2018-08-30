94666e1462bea473ba37eae28ce8238770a05e4c
Tal Ater <tal@talater.com>
2018-08-30 17:34:05 +0300

    Test create need

--- test/server.spec.js
+++ test/server.spec.js
@@ -2,7 +2,7 @@ const { promisify } = require("util");
 const server = require('../server');
 const handler = promisify(server.handler);
 const context = {};
-let event, healthCheckEvent;
+let event, healthCheckEvent, needEvent;
 
 beforeEach(() => {
   event = {
@@ -12,10 +12,18 @@ beforeEach(() => {
     }
   };
   healthCheckEvent = {...event, path: '/healthy'};
+  needEvent = {...event, path: '/need'};
 });
 
 test('health check', async () => {
   const response = await handler(healthCheckEvent, context);
   expect(response.statusCode).toBe(200);
   expect(response.body).toBe('"Hello World!"');
+});
+
+test('create need', async () => {
+  const response = await handler(needEvent, context);
+  const body = JSON.parse(response.body);
+  expect(response.statusCode).toBe(200);
+  expect(body.missionId).toBeGreaterThan(0);
 });
